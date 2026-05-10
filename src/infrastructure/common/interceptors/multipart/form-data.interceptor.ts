12ebf869362b82e3aa310f1a3e392c610c6ace99
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-05-10 15:28:32 -0300

    feat(interceptor): cria FormDataInterceptor para verificar se o cabecalho esta sendo enviado antes de proceguir

---
+++ src/infrastructure/common/interceptors/multipart/form-data.interceptor.ts
@@ -0,0 +1,25 @@
+import {
+  BadRequestException,
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class FormDataInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    const req = context.switchToHttp().getRequest<Request>();
+    const contentType = req.headers['content-type'];
+
+    if (!contentType?.includes('multipart/form-data'))
+      throw new BadRequestException('Required multipart/form-data in header');
+
+    return next.handle();
+  }
+}
--- src/infrastructure/report/controllers/report.controller.ts
+++ src/infrastructure/report/controllers/report.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Post, Req } from '@nestjs/common';
+import { Controller, Post, Req, UseInterceptors } from '@nestjs/common';
 import type { Request } from 'express';
 import { ReportPipeline } from '../../pipelines/report.pipeline';
+import { FormDataInterceptor } from '../../common/interceptors/multipart/form-data.interceptor';
 
 @Controller('report')
 export class ReportController {
+  @UseInterceptors(FormDataInterceptor)
   @Post()
   async sendReport(@Req() req: Request) {
-    const report = await ReportPipeline.generete(req);
-    return report;
+    // const report = await ReportPipeline.generete(req);
+    // return report;
   }
 }
